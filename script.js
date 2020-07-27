94792b9a9eea88ed4fd5ae5ae5958f7bff5c0b39
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-07-26 20:45:12 -0400

    background message attached

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ hamburger.nav.addEventListener('click', function(e) {
 //////////// card functions //////////////
 
 function flipCard() {
+	if (boardLock) return;
 	if (this === firstCard) return;
 	// this.classList.toggle('flip');
 	this.classList.add('flip');
