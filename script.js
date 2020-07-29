daef9e33e1bc24a76b6402d3900bb48210a7037d
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-07-29 16:11:24 -0400

    position fixed to cards only ipad

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function cardsMatch() {
 	secondCard.removeEventListener('click', flipCard);
 
 	count++;
-	if (count === 6) {
+	if (count === 2) {
 		flashBackground();
 	}
 
@@ -89,10 +89,10 @@ function resetBoard() {
 	[ firstCard, secondCard ] = [ null, null ];
 }
 
-(function shuffle() {
-	cards.forEach((card) => {
-		let randomPosition = Math.floor(Math.random() * 12);
-		card.style.order = randomPosition;
-	});
-})();
+// (function shuffle() {
+// 	cards.forEach((card) => {
+// 		let randomPosition = Math.floor(Math.random() * 12);
+// 		card.style.order = randomPosition;
+// 	});
+// })();
 cards.forEach((card) => card.addEventListener('click', flipCard));
