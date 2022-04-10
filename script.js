e502dc002d2c82703e4ab8fd0efedb346e037bf6
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2022-04-10 16:02:02 -0400

    add linkedin info

--- script.js
+++ script.js
@@ -1,125 +1,125 @@
 // console.log(cards);
-const cards = document.querySelectorAll('.card');
-let background = document.querySelector('.background');
-let carButton = document.querySelector('.lilBug');
-let hasCardFlipped = false;
-let boardLock = false;
-let firstCard;
-let secondCard;
-let count = 0;
-let start = false;
-let win;
+const cards = document.querySelectorAll('.card')
+let background = document.querySelector('.background')
+let carButton = document.querySelector('.lilBug')
+let hasCardFlipped = false
+let boardLock = false
+let firstCard
+let secondCard
+let count = 0
+let start = false
+let win
 
 /////////  nav  ///////////
 
 const hamburger = {
-	navToggle: document.querySelector('.nav-toggle'),
-	nav: document.querySelector('nav'),
+  navToggle: document.querySelector('.nav-toggle'),
+  nav: document.querySelector('nav'),
 
-	doToggle: function(e) {
-		e.preventDefault();
-		this.navToggle.classList.toggle('expanded');
-		this.nav.classList.toggle('expanded');
-	}
-};
-hamburger.navToggle.addEventListener('click', function(e) {
-	hamburger.doToggle(e);
-});
-hamburger.nav.addEventListener('click', function(e) {
-	hamburger.doToggle(e);
-});
+  doToggle: function (e) {
+    e.preventDefault()
+    this.navToggle.classList.toggle('expanded')
+    this.nav.classList.toggle('expanded')
+  },
+}
+hamburger.navToggle.addEventListener('click', function (e) {
+  hamburger.doToggle(e)
+})
+hamburger.nav.addEventListener('click', function (e) {
+  hamburger.doToggle(e)
+})
 
 /////////  game ///////////
 
 function flipCard() {
-	if (boardLock) return;
-	if (this === firstCard) return;
-	// this.classList.toggle('flip');
-	this.classList.add('flip');
+  if (boardLock) return
+  if (this === firstCard) return
+  // this.classList.toggle('flip');
+  this.classList.add('flip')
 
-	if (!hasCardFlipped) {
-		hasCardFlipped = true;
-		firstCard = this;
-		return;
-	}
+  if (!hasCardFlipped) {
+    hasCardFlipped = true
+    firstCard = this
+    return
+  }
 
-	secondCard = this;
-	checkForMatch();
+  secondCard = this
+  checkForMatch()
 }
 //do cards match?
 function checkForMatch() {
-	let isMatch = firstCard.dataset.image === secondCard.dataset.image;
+  let isMatch = firstCard.dataset.image === secondCard.dataset.image
 
-	isMatch ? cardsMatch() : cardsDontMatch();
+  isMatch ? cardsMatch() : cardsDontMatch()
 }
 
 function cardsMatch() {
-	firstCard.removeEventListener('click', flipCard);
-	secondCard.removeEventListener('click', flipCard);
+  firstCard.removeEventListener('click', flipCard)
+  secondCard.removeEventListener('click', flipCard)
 
-	count++;
-	if (count === 6) {
-		flashBackground();
-	}
+  count++
+  if (count === 6) {
+    flashBackground()
+  }
 
-	resetBoard();
+  resetBoard()
 }
 
 function flashBackground() {
-	let cardsArray = Array.from(cards);
-	setTimeout(function() {
-		for (i = 0; i < cardsArray.length; i++) {
-			cardsArray[i].children[0].classList.add('show-background');
-			background.classList.add('z-up');
-		}
-	}, 1000);
-	setTimeout(function() {
-		for (i = 0; i < cardsArray.length; i++) {
-			cardsArray[i].children[0].classList.remove('show-background');
-		}
-	}, 10000);
+  let cardsArray = Array.from(cards)
+  setTimeout(function () {
+    for (i = 0; i < cardsArray.length; i++) {
+      cardsArray[i].children[0].classList.add('show-background')
+      background.classList.add('z-up')
+    }
+  }, 1000)
+  setTimeout(function () {
+    for (i = 0; i < cardsArray.length; i++) {
+      cardsArray[i].children[0].classList.remove('show-background')
+    }
+  }, 10000)
 }
 
 function cardsDontMatch() {
-	boardLock = true;
-	setTimeout(() => {
-		firstCard.classList.remove('flip');
-		secondCard.classList.remove('flip');
-		resetBoard();
-	}, 1500);
+  boardLock = true
+  setTimeout(() => {
+    firstCard.classList.remove('flip')
+    secondCard.classList.remove('flip')
+    resetBoard()
+  }, 1500)
 }
 
 function resetBoard() {
-	[ hasCardFlipped, boardLock ] = [ false, false ];
-	[ firstCard, secondCard ] = [ null, null ];
+  ;[hasCardFlipped, boardLock] = [false, false]
+  ;[firstCard, secondCard] = [null, null]
 }
 
 function shuffle() {
-	cards.forEach((card) => {
-		let randomPosition = Math.floor(Math.random() * 12) + 1;
-		card.style.order = randomPosition;
-	});
+  cards.forEach((card) => {
+    let randomPosition = Math.floor(Math.random() * 12) + 1
+    card.style.order = randomPosition
+  })
 }
 
-carButton.addEventListener('click', (event) => {
-	setTimeout(() => {
-		shuffle();
-	}, 500);
-	cards.forEach((card) => {
-		if (card.classList.contains('flip')) {
-			card.classList.remove('flip');
-		}
-	});
-	count = 0;
-	resetBoard();
-	location.reload();
-});
+carButton.addEventListener('click', () => {
+  setTimeout(() => {
+    shuffle()
+  }, 500)
+  cards.forEach((card) => {
+    if (card.classList.contains('flip')) {
+      card.classList.remove('flip')
+    }
+  })
+  count = 0
+  resetBoard()
+  location.reload()
+})
 
 function winGame() {
-	flashBackground();
-	start = false;
-	win = true;
+  flashBackground()
+  start = false
+  win = true
 }
 
-cards.forEach((card) => card.addEventListener('click', flipCard));
-shuffle();
+cards.forEach((card) => card.addEventListener('click', flipCard))
+shuffle()
